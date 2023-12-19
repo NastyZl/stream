1a59057deaecacc30d4b00f78b0fae1bb646344c
Nasty Z <azlatovchena20@mail.ru>
2023-12-19 18:09:31 +0300

    1-9

---
+++ src/main/java/ru/lesson/stream/LessonStreamApi.java
@@ -0,0 +1,155 @@
+package ru.lesson.stream;
+
+import ru.lesson.stream.dto.Employee;
+import ru.lesson.stream.dto.PositionType;
+
+import static java.util.Comparator.comparingInt;
+
+import java.util.HashSet;
+import java.util.List;
+import static java.util.stream.Collectors.*;
+
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class LessonStreamApi {
+
+    /**
+     * Задача №1.
+     * Получить список сотрудников, у которых рейтинг {@link Employee#getRating()} больше 50.
+     * Важно: Необходимо учесть, что List<Employee> employees может содержать дублирующие записи.
+     */
+    public List<Employee> task1(List<Employee> employees) {
+        return employees.stream()
+                .distinct()
+                .filter(employee -> employee.getRating() > 50)
+                .collect(toList());
+    }
+
+    /**
+     * Задача №2.
+     * Получить список сотрудников (List<String> - формат строки {@link Employee#getName()}"="{@link Employee#getRating()})
+     * У которых рейтинг {@link Employee#getRating()} меньше 50.
+     */
+    public List<String> task2(List<Employee> employees) {
+        return employees.stream()
+                .distinct()
+                .filter(employee -> employee.getRating()<50)
+                .map(employee -> employee.getName()+"="+employee.getRating())
+                .collect(toList());
+    }
+
+    /**
+     * Задача №3.
+     * Получить средний рейтнг всех сотрудников.
+     */
+    public double task3(List<Employee> employees) {
+        return employees.stream()
+                .distinct()
+                .mapToDouble(Employee::getRating)
+                .average()
+                .orElse(0);
+    }
+
+    /**
+     * Задача №4.
+     * Получить общий список сотрудников из разных отделов и
+     * отсортировать его по убыванию рейтинга {@link Employee#getRating()}.
+     * Важно: необходимо учесть, что один сотрудник может числиться в разных отделах.
+     * Необходимо устранить дублирование.
+     */
+    public List<Employee> task4(List<List<Employee>> employeeDepartments) {
+        return employeeDepartments.stream()
+                .flatMap(List<Employee>::stream)
+                .distinct()
+                .sorted(comparingInt(Employee::getRating).reversed())
+                .collect(toList());
+    }
+
+    /**
+     * Задача №5.
+     * Предположим, что требуется выводить список сотрудников на веб-страницу.
+     * Необходимо реализовать построчный вывод (пагинацию).
+     * <p>
+     * Пример:
+     * Если number = 1, size = 3, то результирующий список будет
+     * Employee{id=1, name='Name1', rating=11}, Employee{id=2, name='Name2', rating=12}, Employee{id=3, name='Name3', rating=13}
+     * <p>
+     * Если number = 2, size = 3, то результирующий список
+     * Employee{id=4, name='Name4', rating=14}, Employee{id=5, name='Name5', rating=15}, Employee{id=6, name='Name6', rating=16}
+     */
+    public List<Employee> task5(List<Employee> employees, int number, int size) {
+        if (number <= 0) {
+            throw new IllegalArgumentException(Integer.toString(number));
+        }
+        return employees.stream()
+                .skip((number-1)*size)
+                .limit(size)
+                .collect(toList());
+    }
+
+    /**
+     * Задача №6.
+     * Получить имена сотрудников в String: начинается строка с '[',
+     * затем следуют имена сотрудников через ', ' и заканчивается строка ']'.
+     * <p>
+     * Пример результата: [Ivan, Olga, John]
+     */
+    public String task6(List<Employee> employees) {
+        return employees.stream()
+                .distinct()
+                .map(Employee::getName)
+                .collect(joining(", ", "[", "]"));
+    }
+
+    /**
+     * Задача №7.
+     * Проверить наличие дублирующихся имен сотрудников {@link Employee#getName()} в списке.
+     * <p>
+     * Если дубли существуют - вернуть true, если дублей нет - вернуть false
+     */
+    public boolean task7(List<Employee> employees) {
+        Set<String> uniqueNames = new HashSet<>();
+        return employees.stream()
+                .map(Employee::getName)
+                .anyMatch(name ->  !uniqueNames.add(name));
+    }
+
+    /**
+     * Задача №8.
+     * Получить средний рейтинг {@link Employee#getRating()} по каждой
+     * должности сотрудника {@link Employee#getPositionType()}
+     */
+    public Map<PositionType, Double> task8(List<Employee> employees) {
+        return employees.stream().collect(
+                groupingBy(Employee::getPositionType,
+                        Collectors.averagingDouble(Employee::getRating)));
+    }
+
+    /**
+     * Задача 9.
+     * Получить словарь, который содержит две записи:
+     * Ключи: true - эффективные и false - неэффективные сторудники.
+     * Значение: количество сотрудников для каждой из двух групп.
+     * Сотрудник является эффективным, если его рейтинг больше 50.
+     */
+    public Map<Boolean, Long> task9(List<Employee> employees) {
+        return employees.stream()
+                .collect(Collectors.partitioningBy(employee -> employee.getRating() > 50,
+                Collectors.counting()));
+    }
+
+    /**
+     * Задача 10.
+     * Получить словарь, который содержит две записи:
+     * Ключи: true - эффективные и false - неэффективные сотрудники.
+     * Значение: список имен сотрудников через ', '.
+     * Сотрудник является эффективным, если его рейтинг больше 50.
+     */
+    public Map<Boolean, String> task10(List<Employee> employees) {
+       return null;//employees.stream();
+    }
+
+}
